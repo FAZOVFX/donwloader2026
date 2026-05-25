e6287c3c7d000cb3ce5e779d0125fba702c47225
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 18:08:38 +0500

    Update downloader.py

--- app/services/downloader.py
+++ app/services/downloader.py
@@ -1,10 +1,36 @@
 import yt_dlp
+import os
+import uuid
+
+DOWNLOAD_PATH = "downloads"
+
+os.makedirs(
+    DOWNLOAD_PATH,
+    exist_ok=True
+)
+
 
 def download_video(url):
 
+    filename = f"{uuid.uuid4()}.mp4"
+
+    path = os.path.join(
+        DOWNLOAD_PATH,
+        filename
+    )
+
     ydl_opts = {
         "format":
-        "bestvideo+bestaudio"
+        "bestvideo[height<=1080]+bestaudio/best",
+
+        "merge_output_format":
+        "mp4",
+
+        "outtmpl":
+        path,
+
+        "quiet":
+        True
     }
 
     with yt_dlp.YoutubeDL(
@@ -12,3 +38,47 @@ def download_video(url):
     ) as ydl:
 
         ydl.download([url])
+
+    return path
+
+
+def download_mp3(url):
+
+    filename = f"{uuid.uuid4()}.mp3"
+
+    path = os.path.join(
+        DOWNLOAD_PATH,
+        filename
+    )
+
+    ydl_opts = {
+        "format":
+        "bestaudio/best",
+
+        "outtmpl":
+        path,
+
+        "postprocessors": [
+            {
+                "key":
+                "FFmpegExtractAudio",
+
+                "preferredcodec":
+                "mp3",
+
+                "preferredquality":
+                "192"
+            }
+        ],
+
+        "quiet":
+        True
+    }
+
+    with yt_dlp.YoutubeDL(
+        ydl_opts
+    ) as ydl:
+
+        ydl.download([url])
+
+    return path
