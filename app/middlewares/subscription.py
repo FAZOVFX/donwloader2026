16c97167395ebc5cf267b240c2b03f87ef9f7a37
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 17:08:10 +0500

    Update subscription.py

--- app/middlewares/subscription.py
+++ app/middlewares/subscription.py
@@ -1,1 +1,12 @@
-# subscription middleware
+async def check_sub(bot, user_id, channel):
+
+    member = await bot.get_chat_member(
+        channel,
+        user_id
+    )
+
+    return member.status in [
+        "member",
+        "administrator",
+        "creator"
+    ]
