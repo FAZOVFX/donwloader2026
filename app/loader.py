fbc60dcf9b80c277483adc2fdbb0e0c678c1e334
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 18:08:12 +0500

    Update loader.py

--- app/loader.py
+++ app/loader.py
@@ -1,13 +1,6 @@
 from aiogram import Bot, Dispatcher
-from aiogram.enums import ParseMode
-from aiogram.client.default import DefaultBotProperties
 from app.config import BOT_TOKEN
 
-bot = Bot(
-    token=BOT_TOKEN,
-    default=DefaultBotProperties(
-        parse_mode=ParseMode.HTML
-    )
-)
+bot = Bot(token=BOT_TOKEN)
 
 dp = Dispatcher()
