4c06fa03c2969ad4bee1981847c012723f11a782
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 17:08:36 +0500

    Update downloader.py

--- app/services/downloader.py
+++ app/services/downloader.py
@@ -1,1 +1,14 @@
-# downloader service
+import yt_dlp
+
+def download_video(url):
+
+    ydl_opts = {
+        "format":
+        "bestvideo+bestaudio"
+    }
+
+    with yt_dlp.YoutubeDL(
+        ydl_opts
+    ) as ydl:
+
+        ydl.download([url])
