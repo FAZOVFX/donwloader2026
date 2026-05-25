a502f1c79467ad4d5507d374ae7ada1d477f7960
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 17:06:55 +0500

    Update search.py

--- app/handlers/search.py
+++ app/handlers/search.py
@@ -1,2 +1,11 @@
 from aiogram import Router
+from aiogram.types import Message
+
 router = Router()
+
+@router.message()
+async def search_music(message: Message):
+
+    await message.answer(
+        "Qidirilmoqda..."
+    )
