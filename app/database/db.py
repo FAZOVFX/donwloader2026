f4063be499dc64b45c5a723c1f4fe39ca9f5905f
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 17:09:34 +0500

    Update db.py

--- app/database/db.py
+++ app/database/db.py
@@ -1,1 +1,10 @@
-# database connection
+import asyncpg
+
+async def connect_db():
+
+    return await asyncpg.connect(
+        user="postgres",
+        password="password",
+        database="botdb",
+        host="localhost"
+    )
