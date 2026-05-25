f17f59255c74e785d1c395d63b61ac89a4fc5ac9
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 17:05:38 +0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,11 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMINS = list(map(int, os.getenv("ADMINS").split(",")))
+CHANNEL = os.getenv("CHANNEL")
+
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-ACR_HOST = os.getenv("ACR_HOST")
-ACR_KEY = os.getenv("ACR_KEY")
-ACR_SECRET = os.getenv("ACR_SECRET")
