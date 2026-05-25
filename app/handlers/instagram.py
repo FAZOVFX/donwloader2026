eacd2b4e96c29a84a19e74ce6b11bf874d112832
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 18:10:49 +0500

    Update instagram.py

--- app/handlers/instagram.py
+++ app/handlers/instagram.py
@@ -1,13 +1,38 @@
 from aiogram import Router, F
 from aiogram.types import Message
 
+from app.services.downloader import (
+    download_video
+)
+
 router = Router()
+
 
 @router.message(
     F.text.contains("instagram.com")
 )
-async def insta_handler(message: Message):
+async def instagram_download(
+    message: Message
+):
 
-    await message.answer(
+    msg = await message.answer(
         "Instagram video yuklanmoqda..."
     )
+
+    try:
+
+        path = download_video(
+            message.text
+        )
+
+        await message.answer_video(
+            video=path
+        )
+
+        await msg.delete()
+
+    except Exception as e:
+
+        await message.answer(
+            f"Xato: {e}"
+        )
