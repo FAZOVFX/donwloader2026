60f99d07dcd90336ec9c60a29ed7ba84b5c23e7e
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 17:09:55 +0500

    Update users.py

--- app/database/users.py
+++ app/database/users.py
@@ -1,1 +1,14 @@
-# users queries
+async def add_user(
+    db,
+    user_id
+):
+
+    await db.execute(
+
+        '''
+        INSERT INTO users(user_id)
+        VALUES($1)
+        ''',
+
+        user_id
+    )
