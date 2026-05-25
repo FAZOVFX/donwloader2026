b475795e893a93c5ab42a9347f9020b5a60b5197
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 17:07:43 +0500

    Update inline.py

--- app/keyboards/inline.py
+++ app/keyboards/inline.py
@@ -1,1 +1,22 @@
-# inline keyboards
+from aiogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+
+def format_keyboard():
+
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="1080P VIDEO",
+                    callback_data="video"
+                ),
+
+                InlineKeyboardButton(
+                    text="MP3",
+                    callback_data="mp3"
+                )
+            ]
+        ]
+    )
