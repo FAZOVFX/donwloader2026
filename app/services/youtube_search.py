e5aa3a7ee26985565b6e636f1659607aa86a310a
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 17:08:48 +0500

    Update youtube_search.py

--- app/services/youtube_search.py
+++ app/services/youtube_search.py
@@ -1,1 +1,19 @@
-# youtube search service
+import yt_dlp
+
+def search_youtube(query):
+
+    ydl_opts = {
+        "quiet": True,
+        "extract_flat": True
+    }
+
+    with yt_dlp.YoutubeDL(
+        ydl_opts
+    ) as ydl:
+
+        result = ydl.extract_info(
+            f"ytsearch5:{query}",
+            download=False
+        )
+
+    return result["entries"]
