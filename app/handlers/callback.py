c0e39c63d1623922740afce07f166b6661a3a610
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 17:07:20 +0500

    Update callback.py

--- app/handlers/callback.py
+++ app/handlers/callback.py
@@ -1,2 +1,9 @@
 from aiogram import Router
+from aiogram.types import CallbackQuery
+
 router = Router()
+
+@router.callback_query()
+async def callbacks(call: CallbackQuery):
+
+    await call.answer()
