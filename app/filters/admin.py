0dc13f05512163cf425068c3f75ee3a0e804ff5a
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 17:10:10 +0500

    Update admin.py

--- app/filters/admin.py
+++ app/filters/admin.py
@@ -1,1 +1,11 @@
-# admin filter
+from aiogram.filters import BaseFilter
+from app.config import ADMINS
+
+class AdminFilter(BaseFilter):
+
+    async def __call__(self, message):
+
+        return (
+            message.from_user.id
+            in ADMINS
+        )
