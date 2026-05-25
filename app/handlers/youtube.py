aee3c57b7594bfb456a2e07d4e876c89d4bcd442
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 17:06:27 +0500

    Update youtube.py

--- app/handlers/youtube.py
+++ app/handlers/youtube.py
@@ -1,2 +1,15 @@
-from aiogram import Router
+from aiogram import Router, F
+from aiogram.types import Message
+from app.keyboards.inline import format_keyboard
+
 router = Router()
+
+@router.message(
+    F.text.contains("youtube.com")
+)
+async def youtube_handler(message: Message):
+
+    await message.answer(
+        "Format tanlang",
+        reply_markup=format_keyboard()
+    )
