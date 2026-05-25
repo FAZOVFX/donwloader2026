0593b2839fb990d1a3023820534b3e059a1ea0d5
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 17:49:37 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 import asyncio
+import threading
+import os
+
+from flask import Flask
 
 from app.loader import (
     bot,
@@ -12,6 +16,7 @@ from app.handlers.search import router as search_router
 from app.handlers.admin import router as admin_router
 from app.handlers.callback import router as callback_router
 
+# Routerlar
 dp.include_router(start_router)
 dp.include_router(youtube_router)
 dp.include_router(insta_router)
@@ -19,9 +24,45 @@ dp.include_router(search_router)
 dp.include_router(admin_router)
 dp.include_router(callback_router)
 
-async def main():
+# Flask app
+web = Flask(__name__)
+
+@web.route("/")
+def home():
+    return "Bot is running"
+
+
+# Web server
+def run_web():
+
+    port = int(
+        os.environ.get(
+            "PORT",
+            10000
+        )
+    )
+
+    web.run(
+        host="0.0.0.0",
+        port=port
+    )
+
+
+# Telegram bot
+async def run_bot():
 
     await dp.start_polling(bot)
 
+
+# Main
 if __name__ == "__main__":
-    asyncio.run(main())
+
+    # Flask thread
+    threading.Thread(
+        target=run_web
+    ).start()
+
+    # Bot polling
+    asyncio.run(
+        run_bot()
+    )
