37a17dfa60b37f9723f306e3e4cbc2cf4e80b919
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 17:07:06 +0500

    Update admin.py

--- app/handlers/admin.py
+++ app/handlers/admin.py
@@ -1,2 +1,13 @@
 from aiogram import Router
+from aiogram.types import Message
+from app.config import ADMINS
+
 router = Router()
+
+@router.message()
+async def admin_panel(message: Message):
+
+    if message.from_user.id in ADMINS:
+        await message.answer(
+            "Admin panel"
+        )
