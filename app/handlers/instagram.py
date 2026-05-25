69b76527a378ccfa1302543f7ecbd2e3f7fe3e24
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 17:06:43 +0500

    Update instagram.py

--- app/handlers/instagram.py
+++ app/handlers/instagram.py
@@ -1,2 +1,13 @@
-from aiogram import Router
+from aiogram import Router, F
+from aiogram.types import Message
+
 router = Router()
+
+@router.message(
+    F.text.contains("instagram.com")
+)
+async def insta_handler(message: Message):
+
+    await message.answer(
+        "Instagram video yuklanmoqda..."
+    )
