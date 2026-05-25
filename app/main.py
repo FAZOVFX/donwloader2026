0f0d3ef8bccfb95b4a3d05d6f944c08e74673ed9
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 18:09:59 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,26 +9,30 @@ from app.loader import (
     dp
 )
 
-from app.handlers.start import router as start_router
-from app.handlers.youtube import router as youtube_router
-from app.handlers.instagram import router as insta_router
-from app.handlers.search import router as search_router
-from app.handlers.admin import router as admin_router
-from app.handlers.callback import router as callback_router
+from app.handlers.start import (
+    router as start_router
+)
 
-# Routerlar
+from app.handlers.search import (
+    router as search_router
+)
+
+from app.handlers.callback import (
+    router as callback_router
+)
+
+# Routers
 dp.include_router(start_router)
-dp.include_router(youtube_router)
-dp.include_router(insta_router)
 dp.include_router(search_router)
-dp.include_router(admin_router)
 dp.include_router(callback_router)
 
 # Flask app
 web = Flask(__name__)
 
+
 @web.route("/")
 def home():
+
     return "Bot is running"
 
 
@@ -57,12 +61,10 @@ async def run_bot():
 # Main
 if __name__ == "__main__":
 
-    # Flask thread
     threading.Thread(
         target=run_web
     ).start()
 
-    # Bot polling
     asyncio.run(
         run_bot()
     )
