c012a454938f0650e7e14a4068cf7add7e573f38
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2026-05-25 17:07:56 +0500

    Update reply.py

--- app/keyboards/reply.py
+++ app/keyboards/reply.py
@@ -1,1 +1,22 @@
-# reply keyboards
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton
+)
+
+admin_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(
+                text="STATISTIKA"
+            )
+        ],
+
+        [
+            KeyboardButton(
+                text="BROADCAST"
+            )
+        ]
+    ],
+
+    resize_keyboard=True
+)
